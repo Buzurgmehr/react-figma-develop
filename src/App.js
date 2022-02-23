bc6cbee8f5c5c0f22c298b173eed5fa32071e6a5
Buzurgmehr <kholiqovbuzurgmehr59@gmail.com>
2022-02-23 16:19:17 +0500

    commit

--- src/App.js
+++ src/App.js
@@ -1,27 +1,27 @@
 import React from "react";
-import ReactDOM from "react-dom";
 import Home from "./View/Header/Home";
 import Logo from "./View/Header/Logo";
 import Overview from "./View/Header/Overview";
 import Pages from "./View/Header/Pages";
 import Portfolio from "./View/Header/Portfolio";
 import Template from "./View/Header/Template";
-
-import {Routes, Route} from "react-router-dom";
-
+import { Routes, Route } from "react-router-dom";
 import "./index.css";
 
 function App() {
-    return (
-        <div className="App">
-            <Home/>
-            <Logo/>
-            <Overview/>
-            <Pages/>
-            <Portfolio/>
-            <Template/>
-        </div>
-    );
+  return (
+    <div className="App">
+      <Home />
+      <Routes>
+        <Route path="/" exact element={<Home />} />
+        <Route path="/logo" element={<Logo />} />
+        <Route path="/overview" element={<Overview />} />
+        <Route path="/pages" element={<Pages />} />
+        <Route path="/portfolio" element={<Portfolio />} />
+        <Route path="/template" element={<Template />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
--- src/View/Header/Logo.jsx
+++ src/View/Header/Logo.jsx
@@ -2,7 +2,15 @@ import React from 'react'
 
 const Logo = () => {
   return (
-    <div>Logo</div>
+    <div className='ComponentsLogo'>
+      <div className='ComponentsLogo-image'>
+        <div className='ComponentsLogo-image__title'>
+
+        </div>
+        <img src="" alt="" />
+      </div>
+      Logo
+    </div>
   )
 }
 
