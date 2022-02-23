59ec2639f0bd2532378dac1eddf0c117bb6b09a6
Buzurgmehr <kholiqovbuzurgmehr59@gmail.com>
2022-02-23 13:54:51 +0500

    commit

--- src/App.js
+++ src/App.js
@@ -1,15 +1,27 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import {Routes, Route} from 'react-router-dom';
+import React from "react";
+import ReactDOM from "react-dom";
+import Home from "./View/Header/Home";
+import Logo from "./View/Header/Logo";
+import Overview from "./View/Header/Overview";
+import Pages from "./View/Header/Pages";
+import Portfolio from "./View/Header/Portfolio";
+import Template from "./View/Header/Template";
 
-import './index.css';
+import {Routes, Route} from "react-router-dom";
+
+import "./index.css";
 
 function App() {
-  return (
-    <div className="App">
-     <h1>Done!</h1>
-    </div>
-  );
+    return (
+        <div className="App">
+            <Home/>
+            <Logo/>
+            <Overview/>
+            <Pages/>
+            <Portfolio/>
+            <Template/>
+        </div>
+    );
 }
 
 export default App;
